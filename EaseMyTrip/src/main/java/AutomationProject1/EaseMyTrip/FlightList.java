88d82526550c00e2a33a6640a7b34d93c320c00a
savithaselvaraj <48199069+savithaselvaraj@users.noreply.github.com>
2022-01-07 21:16:57 -0800

    Added FlightList.java and modified HomePage.java to modify # of travellers and class of travel.

---
+++ EaseMyTrip/src/main/java/AutomationProject1/EaseMyTrip/FlightList.java
@@ -0,0 +1,42 @@
+package AutomationProject1.EaseMyTrip;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FlightList {
+	
+	private WebDriver driver;
+	
+	public FlightList(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void getFlights() {
+		System.out.println(driver.getCurrentUrl());
+
+		List<WebElement> flightNames=(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//span[@class='txt-r4 ng-binding']")));
+
+		List<WebElement> flightTimes =  driver.findElements(By.xpath("//span[@class='txt-r2-n ng-binding']"));
+		
+		List<WebElement> flightPrices = driver.findElements(By.xpath("//div[contains(@id,'spnPrice')]"));
+		
+		System.out.println("There are "+flightNames.size()+" flights.");
+		System.out.println("There are "+flightTimes.size()+" flight timings");
+		
+		for(int i=0, j=0; i<flightNames.size();i++,j=j+2) {
+			System.out.print(flightNames.get(i).getText()+"\t"+flightTimes.get(j).getText()+"\t"+flightTimes.get(j+1).getText()+"\t"+flightPrices.get(j).getText());
+			System.out.println();
+		}
+		
+//		for (WebElement flightName: flightNames) {
+//			System.out.println(flightName.getText());
+//		}
+		
+
+	}	
+}
--- EaseMyTrip/src/main/java/AutomationProject1/EaseMyTrip/HomePage.java
+++ EaseMyTrip/src/main/java/AutomationProject1/EaseMyTrip/HomePage.java
@@ -18,7 +18,7 @@ public class HomePage {
 		goToHomePage();
 		String fromStr = "Mumbai";
 		String toStr = "Chennai";
-		String month = "DEC";
+		String month = "NOV 2022";
 		WebElement from = driver.findElement(By.id("FromSector_show"));
 		from.click();
 		from.sendKeys(fromStr);
@@ -46,7 +46,56 @@ public class HomePage {
 		WebElement dayElement = driver.findElement(By.xpath("//div[@class='days']//li[contains(text(),'15')]"));
 		dayElement.click();
 		
-		WebElement searchBtn = driver.findElement(By.id("search")); 
-		searchBtn.click();
+		//Chaaru's code
+		
+		WebElement Travellerdropddown = driver.findElement(By.xpath("//*[@id=\"frmHome\"]/div[5]/div[2]/div[3]/div[1]/div[5]/div/a/span"));
+		Travellerdropddown.click();
+		
+		travellers(2, 1, 1);
+		
+		String travClass= "Prem.Economy";
+		driver.findElement(By.xpath("//*[@id='frmHome']/div[5]/div[2]/div[3]/div[1]/div[6]/div/a/span")).click();		
+		driver.findElement(By.xpath("//label[contains(text(),'" + travClass + "')]/input[@name='optClass']")).click();	
+		driver.findElement(By.xpath("//a[@id='tripType']")).click();
+		driver.findElement(By.xpath("//*[@id='search']//input")).click();
+		
+		//End Chaaru's code
+		
+		FlightList fl = new FlightList(driver);
+		fl.getFlights();
+	}
+	
+
+	public static void travellers(int adult, int children, int infants) {
+	
+		WebElement Adultvalue = driver.findElement(By.id("optAdult"));
+		int displayvalue = Integer.parseInt(Adultvalue.getAttribute("value"));
+		WebElement Adultminus = driver.findElement(By.xpath("//*[@id=\"myDropdown_n\"]/div/div[1]/div[2]/div/div[1]/input"));
+		WebElement Adultplus = driver.findElement(By.xpath("//*[@id=\"myDropdown_n\"]/div/div[1]/div[2]/div/div[3]/input"));
+		
+		while(displayvalue<adult) {
+			Adultplus.click();
+			displayvalue = Integer.parseInt(Adultvalue.getAttribute("value"));
+		}
+		
+		WebElement Childrenvalue = driver.findElement(By.id("optChild"));
+		displayvalue = Integer.parseInt(Childrenvalue.getAttribute("value"));
+		WebElement Childrenplus = driver.findElement(By.xpath("//*[@id=\"myDropdown_n\"]/div/div[2]/div[2]/div/div[3]/input"));
+		
+		while(displayvalue<children) {
+			Childrenplus.click();
+			displayvalue = Integer.parseInt(Childrenvalue.getAttribute("value"));
+		}
+		
+		WebElement infantsvalue = driver.findElement(By.id("optInfant"));
+		displayvalue = Integer.parseInt(infantsvalue.getAttribute("value"));
+		WebElement infantsplus = driver.findElement(By.xpath("//*[@id=\"myDropdown_n\"]/div/div[3]/div[2]/div/div[3]/input"));
+		
+		while(displayvalue<infants) {
+			infantsplus.click();
+			displayvalue = Integer.parseInt(infantsvalue.getAttribute("value"));
+		}
+		
+		driver.findElement(By.xpath("//*[@id=\"traveLer\"]")).click();
 	}
 }
